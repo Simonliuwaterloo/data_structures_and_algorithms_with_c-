126221a5f968a35b5bc35c597135daf981fae250
Jianyan Li <simonli1215@outlook.com>
2019-06-06 20:14:06 -0400

    add stl vector implementation

--- chapter_6/6.1_vectors/main.cpp
+++ chapter_6/6.1_vectors/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "ArrayVector.h"
-
+#include <vector>
+//STL vector
+void stl_vector_sample() {
+    std::vector<int> stl_vector(100);
+    std::cout << stl_vector[2]<< std::endl;
+    //initialized to 0
+    std::cout << stl_vector.at(2)<< std::endl;
+    //member function at, permorms range chacking
+    //std::cout << stl_vector.at(2)<< std::endl;
+    //throws an error
+    stl_vector.at(2) = 3;
+    std::cout << stl_vector.at(2)<< std::endl;
+    //at returns a reference
+    std::cout << stl_vector.size()<< std::endl;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    stl_vector_sample();
     return 0;
 }
